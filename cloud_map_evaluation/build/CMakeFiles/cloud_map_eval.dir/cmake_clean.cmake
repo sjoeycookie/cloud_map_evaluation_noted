fd8ed0c386e1a9a7e34bbb17b4474f6ab8e884be
sjoeycookie <sjoeycookie@163.com>
2024-11-19 09:49:23 +0800

    first commit

---
+++ cloud_map_evaluation/build/CMakeFiles/cloud_map_eval.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloud_map_eval.dir/src/cloud_map_eval.cpp.o"
+  "CMakeFiles/cloud_map_eval.dir/src/cloud_map_eval.cpp.o.d"
+  "CMakeFiles/cloud_map_eval.dir/src/cloud_map_eval_node.cpp.o"
+  "CMakeFiles/cloud_map_eval.dir/src/cloud_map_eval_node.cpp.o.d"
+  "cloud_map_eval"
+  "cloud_map_eval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cloud_map_eval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
